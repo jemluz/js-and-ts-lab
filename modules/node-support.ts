bb4b9223727f57de2a0c539c44b7e20db028ac9e
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-01-10 00:18:56 +0000

    feat: add node support file to modules/ folder

---
+++ modules/node-support.ts
@@ -0,0 +1,11 @@
+// Node.js supports both CommonJS and ESM module systems.
+
+// const fs = require('fs');     // CommonJS
+// import fs from 'fs';          // ESM
+
+// You can use type keywords at package.json to differentiate between the two module systems.
+
+// package.json
+// {
+//   "type": "module"
+// }
